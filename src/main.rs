32f294be05d2b34eaf2cca5e083523e4d49fdca9
Casey Rodarmor <casey@rodarmor.com>
2021-04-20 14:07:09 -0700

    Send and print message

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
-use std::{
-  env,
-  io::ErrorKind,
-  net::{IpAddr, UdpSocket},
-  thread,
-  time::Duration,
-};
+use std::{env, io::ErrorKind, net::UdpSocket, thread, time::Duration};
 
 fn main() {
-  let address = env::args()
-    .into_iter()
-    .skip(1)
-    .next()
-    .unwrap()
-    .parse::<IpAddr>()
-    .unwrap();
+  let args = env::args().into_iter().collect::<Vec<String>>();
+
+  let address = &args[1];
+
+  let message = &args[2];
 
   let port = 5546;
 
@@ -26,10 +18,10 @@ fn main() {
   // Poll for data every 5 milliseconds for 5 seconds.
   let mut buffer = [0u8; 1024];
 
-  let message = b"hello";
-
   loop {
-    let sent = socket.send_to(message, (address, port)).unwrap();
+    let sent = socket
+      .send_to(message.as_bytes(), (address.as_str(), port))
+      .unwrap();
     assert_eq!(sent, message.len());
 
     let result = socket.recv(&mut buffer);
@@ -37,7 +29,7 @@ fn main() {
     match result {
       // If `recv` was successfull, print the number of bytes received.
       // The received data is stored in `buffer`.
-      Ok(num_bytes) => println!("I received {} bytes!", num_bytes),
+      Ok(_) => println!("Message recieved: {}", String::from_utf8_lossy(&buffer)),
       // If we get an error other than "would block", print the error.
       Err(ref err) if err.kind() != ErrorKind::WouldBlock => {
         println!("Something went wrong: {}", err)
